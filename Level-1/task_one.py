127b56599db3ea086bc5a65c32af5dd3b714bea2
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-11-08 14:30:29 +0000

    passing pytests

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,4 +1,47 @@
-from datetime import date
+from datetime import datetime, date
+
+
+class Assessment:
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        if type not in ['multiple-choice', 'technical', 'presentation']:
+            raise ValueError("Invalid assessment type")
+        self.type = type
+        if score > 100 or score < 0:
+            raise ValueError("Invalid score")
+        self.score = score
+
+
+
+class Trainee:
+    def __init__(self, name: str, email: str, date_of_birth: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+
+    def get_age(self) -> int:
+        """returns the age of the trainee in years"""
+        year_difference = int(datetime.now().year) - int(self.date_of_birth.year)
+        month_difference = int(datetime.now().month) - int(self.date_of_birth.month)
+        day_difference = int(datetime.now().day) - int(self.date_of_birth.day)
+        if month_difference >= 0:
+            if day_difference >= 0:
+                return year_difference
+        return year_difference - 1
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        self.assessments.append(assessment)
+
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+        return None
+
+
 
 
 
