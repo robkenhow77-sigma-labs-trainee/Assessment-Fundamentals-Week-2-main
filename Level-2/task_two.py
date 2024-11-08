a047a862b3556e790a41f2b631a57992f6a77793
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-11-08 14:53:08 +0000

    completed task 2

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -1,18 +1,83 @@
-from datetime import date
+from datetime import date, datetime
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 BELOW
-#
-#####
+class Assessment:
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        if type not in ['multiple-choice', 'technical', 'presentation']:
+            raise ValueError("Invalid assessment type")
+        self.type = type
+        if score > 100 or score < 0:
+            raise ValueError("Invalid score")
+        self.score = score
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 ABOVE
-#
-#####
+
+class Trainee:
+    def __init__(self, name: str, email: str, date_of_birth: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+
+    def get_age(self) -> int:
+        """returns the age of the trainee in years"""
+        year_difference = int(datetime.now().year) - int(self.date_of_birth.year)
+        month_difference = int(datetime.now().month) - int(self.date_of_birth.month)
+        day_difference = int(datetime.now().day) - int(self.date_of_birth.day)
+        if month_difference >= 0:
+            if day_difference >= 0:
+                return year_difference
+        return year_difference - 1
+
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        if not isinstance(assessment, Assessment):
+            raise TypeError("Invalid object")
+        self.assessments.append(assessment)
+
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+        return None
+    
+
+    def get_assessment_of_type(self, type: str) -> list[Assessment]:
+        return [assessment for assessment in self.assessments if assessment.type == type]
+
+
+class MultipleChoiceAssessment(Assessment):
+    def __init__(self, name, score):
+        super().__init__(name = name, score = score, type = "multiple-choice")
+
+
+
+    def calculate_score(self) -> float:
+        return self.score * 0.7
+
+
+class TechnicalAssessment(Assessment):
+    def __init__(self, name, score):
+        super().__init__(name = name, score = score, type = "technical")
+
+
+    def calculate_score(self) -> float:
+        return self.score
+
+
+
+class PresentationAssessment(Assessment):
+    def __init__(self, name, score):
+        super().__init__(name = name, score = score, type = "presentation")
+
+
+    def calculate_score(self) -> float:
+        return self.score * 0.6
+
+
 
 
 if __name__ == "__main__":
